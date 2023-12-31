158dfb41fa8e285e1a8e048ccc3971fc8e3f9a05
golo230 <92342157+golo230@users.noreply.github.com>
2023-12-30 17:12:20 -0800

    Add files via upload

---
+++ responses.py
@@ -0,0 +1,12 @@
+def handle_response(message) -> str:
+    p_message = message.lower()
+
+    if p_message == 'hello':
+        return 'Hello world!'
+    
+    if p_message == 'roll':
+        return 'im not mudae bruh :skull: '
+    
+    if p_message == 'test':
+        return 'testing'
+    
